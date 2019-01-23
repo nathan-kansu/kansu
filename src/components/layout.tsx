61411aa805590fb0e67370b8b5174cfe9b1affa5
Nathan Tredgett <nathantredgett@gmail.com>
2019-01-23 16:03:20 +0000

    added styled-components theme

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,12 @@
 import { graphql, StaticQuery } from 'gatsby'
 import * as React from 'react'
-import styled from 'styled-components'
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 import Footer from './footer'
 import Header from './header'
 import Main from './main'
 
-import './layout.css'
+import theme from '../constants/theme'
 
 interface LayoutProps {
   children: JSX.Element[]
@@ -15,6 +15,21 @@ interface LayoutProps {
 const StyledLayout = styled.div`
   margin: 0 auto;
   max-width: 960px;
+`
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: ${props => props.theme.colors.black};
+        color: ${props => props.theme.colors.white};
+    }
+
+    *, *:before, *:after {
+    box-sizing: inherit;
+    }
 `
 
 const Layout = ({ children }: LayoutProps) => (
@@ -29,11 +44,14 @@ const Layout = ({ children }: LayoutProps) => (
       }
     `}
     render={data => (
-      <StyledLayout>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <Main>{children}</Main>
-        <Footer />
-      </StyledLayout>
+      <ThemeProvider theme={theme}>
+        <StyledLayout>
+          <GlobalStyle />
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <Main>{children}</Main>
+          <Footer />
+        </StyledLayout>
+      </ThemeProvider>
     )}
   />
 )
---
+++ src/constants/theme.js
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    black: '#14212c',
+    grey: '#49555e',
+    white: '#ffffff',
+  },
+  fonts: {
+    body: '"Roboto", sans-serif',
+    heading: '"Montserrat", sans-serif',
+  },
+}
