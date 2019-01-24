8c389b1584a5332055c2c70a04319761f7827634
Nathan Tredgett <nathantredgett@gmail.com>
2019-01-24 15:11:32 +0000

    added "text" component

---
+++ src/components/text.tsx
@@ -0,0 +1,17 @@
+import { modularScale } from 'polished'
+import * as React from 'react'
+import styled from 'styled-components'
+
+const StyledText = styled.div`
+  font-size: ${modularScale(1)};
+  line-height: 1.5;
+  max-width: 36em;
+`
+
+interface TextProps {
+  children: string | JSX.Element
+}
+
+const Text = ({ children }: TextProps) => <StyledText>{children}</StyledText>
+
+export default Text
