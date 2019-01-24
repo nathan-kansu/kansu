492b6526fecddb9ddad4f13a58538bab90da6d59
Nathan Tredgett <nathantredgett@gmail.com>
2019-01-24 15:44:45 +0000

    added ButtonLink

---
+++ src/components/button-link.tsx
@@ -0,0 +1,24 @@
+import { modularScale } from 'polished'
+import * as React from 'react'
+import styled from 'styled-components'
+
+interface ButtonLinkProps {
+  href: string
+  title: string
+}
+
+const StyledButton = styled.a`
+  border: solid 3px ${props => props.theme.colors.white};
+  color: ${props => props.theme.colors.white};
+  font-family: ${props => props.theme.fonts.heading};
+  font-size: ${modularScale(1)};
+  padding: ${modularScale(0)};
+  text-decoration: none;
+  text-transform: uppercase;
+`
+
+const ButtonLink = ({ href, title }: ButtonLinkProps) => (
+  <StyledButton href={href}>{title}</StyledButton>
+)
+
+export default ButtonLink
