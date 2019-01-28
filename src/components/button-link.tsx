651e64729a5e6a971834e4fbd94ab2b21895fade
Nathan Tredgett <nathantredgett@gmail.com>
2019-01-28 11:56:57 +0000

    adjusted Button styling

--- src/components/button-link.tsx
+++ src/components/button-link.tsx
@@ -13,12 +13,38 @@ const StyledButton = styled.a`
   font-family: ${props => props.theme.fonts.heading};
   font-size: ${modularScale(1)};
   padding: ${modularScale(0)};
+  position: relative;
   text-decoration: none;
   text-transform: uppercase;
+  transition: all 0.25s ease-in-out;
+
+  &:before {
+    border: solid 20px ${props => props.theme.colors.black};
+    content: '';
+    position: absolute;
+    left: -20px;
+    top: -20px;
+  }
+
+  &:after {
+    border: solid 20px ${props => props.theme.colors.black};
+    bottom: -20px;
+    content: '';
+    position: absolute;
+    right: -20px;
+  }
 
   &:hover {
     background: ${props => props.theme.colors.white};
     color: ${props => props.theme.colors.darkBlue};
+
+    &:before {
+      border: 0;
+    }
+
+    &:after {
+      border: 0;
+    }
   }
 `
 
