43ff56310c9bfb46af8afa46bbb69033c07549e6
Nathan Tredgett <nathantredgett@gmail.com>
2019-02-12 19:09:39 +0000

    refactored Hero to use hooks

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,59 +1,45 @@
 import { rem } from 'polished'
-import React, { Component, createRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import Animation from './animation'
-
-interface HeroState {
-  height: number
-  width: number
-}
 
 const StyledWrapper = styled.div`
   height: ${rem(424)};
   width: 100%;
 `
 
-class Hero extends Component<{}, HeroState> {
-  private wrapperRef = createRef<HTMLDivElement>()
+const Hero = () => {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+  const [heroDimensions, setHeroDimensions] = useState({
+    height: 0,
+    width: 0,
+  })
 
-  constructor(props: {}) {
-    super(props)
+  const handleResize = () => {
+    if (wrapperRef.current) {
+      const { offsetHeight, offsetWidth } = wrapperRef.current
 
-    this.state = {
-      height: 424,
-      width: 980,
+      setHeroDimensions({
+        height: offsetHeight,
+        width: offsetWidth,
+      })
     }
   }
 
-  public componentDidMount() {
-    this.updateCanvasDimensions()
-    window.addEventListener('resize', this.updateCanvasDimensions)
-  }
+  useEffect(() => {
+    handleResize()
 
-  public componentWillUnmount() {
-    window.removeEventListener('resize', this.updateCanvasDimensions)
-  }
-
-  public render = () => {
-    return (
-      <StyledWrapper ref={this.wrapperRef}>
-        <Animation {...this.state} />
-      </StyledWrapper>
-    )
-  }
-
-  private updateCanvasDimensions = () => {
-    if (!this.wrapperRef.current) {
-      return
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
     }
+  }, [])
 
-    const { offsetHeight: height, offsetWidth: width } = this.wrapperRef.current
-
-    this.setState({
-      height,
-      width,
-    })
-  }
+  return (
+    <StyledWrapper ref={wrapperRef}>
+      {heroDimensions.width && <Animation {...heroDimensions} />}
+    </StyledWrapper>
+  )
 }
 
 export default Hero
