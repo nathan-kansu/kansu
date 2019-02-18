49fc0cfe7c284b1a02eca8efbed6d4e3cc87e1ec
Nathan Tredgett <nathantredgett@gmail.com>
2019-02-18 15:52:36 +0000

    added animation to Text

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,8 +1,9 @@
 import { rem } from 'polished'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { animated, useSpring } from 'react-spring'
 import styled from 'styled-components'
 
-const StyledText = styled.div`
+const StyledText = styled(animated.div)`
   align-self: center;
   font-size: ${rem(18)};
   line-height: ${rem(24)};
@@ -13,8 +14,23 @@ interface TextProps {
   className?: string
 }
 
-const Text = ({ className, children }: TextProps) => (
-  <StyledText className={className}>{children}</StyledText>
-)
+const Text = ({ className, children }: TextProps) => {
+  const styleProps = useSpring({
+    from: {
+      opacity: 0,
+      transform: `translateY(${rem(24)})`,
+    },
+    to: {
+      opacity: 1,
+      transform: 'translateY(0)',
+    },
+  })
+
+  return (
+    <StyledText style={styleProps} className={className}>
+      {children}
+    </StyledText>
+  )
+}
 
 export default Text
