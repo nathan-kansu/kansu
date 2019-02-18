227c89a455af0ee97a9c51de3d5cdd79d4151e20
Nathan Tredgett <nathantredgett@gmail.com>
2019-02-18 13:33:20 +0000

    added ButtonTextLink to Contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import { rem } from 'polished'
+
+import ButtonTextLink from '../components/btn-text-link'
 import Container from '../components/container'
 import { H2 } from '../components/heading'
 import Section from '../components/section'
 import SEO from '../components/seo'
 import Text from '../components/text'
+
+const StyledButtonTextLink = styled(ButtonTextLink)`
+  align-self: center;
+`
+
+const StyledText = styled(Text)`
+  margin-bottom: ${rem(48)};
+`
 
 const ContactPage = () => (
   <>
@@ -11,12 +23,13 @@ const ContactPage = () => (
     <Section>
       <H2>Contact</H2>
       <Container>
-        <Text>
+        <StyledText>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
           fugiat unde architecto incidunt rerum temporibus magni odio nam, modi
           harum magnam voluptatum quo! Voluptatibus tenetur soluta numquam.
           Officia, incidunt velit!
-        </Text>
+        </StyledText>
+        <StyledButtonTextLink href="mailto:hello@kansu.co" title="Contact" />
       </Container>
     </Section>
   </>
