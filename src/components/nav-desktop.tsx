5948d962a26c843402e036242a5313193139ff17
Nathan Tredgett <nathantredgett@gmail.com>
2019-02-19 11:18:15 +0000

    added NavLinkDesktop

--- src/components/nav-desktop.tsx
+++ src/components/nav-desktop.tsx
@@ -1,7 +1,8 @@
-import { Link } from 'gatsby'
 import { rem } from 'polished'
 import React from 'react'
 import styled from 'styled-components'
+
+import NavLinkDesktop from '../components/nav-link-desktop'
 import { NAV } from '../constants'
 
 const StyledNavDesktop = styled.nav`
@@ -24,38 +25,12 @@ const StyledLi = styled.li`
   }
 `
 
-const StyledLink = styled(Link)`
-  color: ${props => props.theme.colors.white};
-  display: block;
-  font-family: ${props => props.theme.fonts.heading};
-  font-weight: bold;
-  font-size: 1rem;
-  letter-spacing: 2.3px;
-  position: relative;
-  text-transform: uppercase;
-
-  &.active {
-    position: relative;
-
-    &:after {
-      border-bottom: solid ${rem(6)} ${props => props.theme.colors.white};
-      bottom: -${rem(12)};
-      content: '';
-      left: 0;
-      position: absolute;
-      right: 0;
-    }
-  }
-`
-
 const Nav = () => (
   <StyledNavDesktop>
     <StyledUl>
       {NAV.map(({ title, url }) => (
         <StyledLi key={title}>
-          <StyledLink to={url} activeClassName="active">
-            {title}
-          </StyledLink>
+          <NavLinkDesktop url={url} title={title} />
         </StyledLi>
       ))}
     </StyledUl>
---
+++ src/components/nav-link-desktop.tsx
@@ -0,0 +1,48 @@
+import { Link } from 'gatsby'
+import { rem } from 'polished'
+import React from 'react'
+import styled from 'styled-components'
+
+interface NavLinkDesktopProps {
+  title: string
+  url: string
+}
+
+const StyledNavLinkDesktop = styled(Link)`
+  color: ${props => props.theme.colors.grey};
+  display: block;
+  font-family: ${props => props.theme.fonts.heading};
+  font-weight: bold;
+  font-size: 1rem;
+  letter-spacing: 2.3px;
+  position: relative;
+  text-transform: uppercase;
+  transition: color 0.1s;
+
+  &:hover {
+    color: ${props => props.theme.colors.white};
+  }
+
+  &.active {
+    color: ${props => props.theme.colors.white};
+    position: relative;
+
+    &:after {
+      border-bottom: solid ${rem(6)} ${props => props.theme.colors.white};
+      bottom: -${rem(12)};
+      content: '';
+      left: 0;
+      position: absolute;
+      right: 0;
+    }
+  }
+`
+const NavLinkdesktop = ({ title, url }: NavLinkDesktopProps) => {
+  return (
+    <StyledNavLinkDesktop activeClassName="active" to={url}>
+      {title}
+    </StyledNavLinkDesktop>
+  )
+}
+
+export default NavLinkdesktop
