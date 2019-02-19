309131990b97544a5201c88fdbab1a87bfcdefc8
Nathan Tredgett <nathantredgett@gmail.com>
2019-02-19 19:11:46 +0000

    rounded values returned from shape util methods

--- src/utils/shape.ts
+++ src/utils/shape.ts
@@ -5,24 +5,23 @@ import {
   COLOR_WHITE_RGBA,
 } from '../constants'
 
-import { Shape } from '../interfaces'
-
 const colors: [number[], number[], number[]] = [
   COLOR_DARK_BLUE_RGBA,
   COLOR_GREY_RGBA,
   COLOR_WHITE_RGBA,
 ]
 
-const generateDimension = () => Math.random() * ANIMATION_MAX_DIMENSION
+const generateDimension = () =>
+  Math.round(Math.random() * ANIMATION_MAX_DIMENSION)
 
 const generateFill = () => {
   const fillColorKey = Math.floor(Math.random() * colors.length)
   return colors[fillColorKey]
 }
 
-const generatePosition = (limit: any, dimension: any) => {
+const generatePosition = (limit: number, dimension: number) => {
   const edge = limit - dimension
-  return Math.random() * edge
+  return Math.round(Math.random() * edge)
 }
 
 export const generateShapeFactory = (
