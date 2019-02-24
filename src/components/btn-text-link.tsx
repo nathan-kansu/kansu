a38945035158b0a24ae68e5e788ae0286fc422c9
Nathan Tredgett <nathantredgett@gmail.com>
2019-02-24 16:11:29 +0000

    tweaked Contact button animation

--- src/components/btn-text-link.tsx
+++ src/components/btn-text-link.tsx
@@ -1,8 +1,7 @@
 import { rem } from 'polished'
-import React, { useState } from 'react'
+import React from 'react'
 import { animated, useSpring } from 'react-spring'
 import styled from 'styled-components'
-import theme from '../styles/theme'
 
 interface ButtonTextLinkProps {
   className?: string
@@ -11,14 +10,7 @@ interface ButtonTextLinkProps {
 }
 
 const StyledButton = styled(animated.a)`
-  background-position: 0 0;
-  background-size: 100% 200%;
-  background-image: linear-gradient(
-    ${props => props.theme.colors.white},
-    ${props => props.theme.colors.white} 50%,
-    transparent 50%,
-    transparent
-  );
+  background: ${props => props.theme.colors.white};
   border: solid ${rem(6)} ${props => props.theme.colors.white};
   color: ${props => props.theme.colors.darkBlue};
   font-family: ${props => props.theme.fonts.heading};
@@ -29,24 +21,28 @@ const StyledButton = styled(animated.a)`
   position: relative;
   text-decoration: none;
   text-transform: uppercase;
+  transition: background 0.3s, color 0.3s;
+
+  &:hover {
+    background: none;
+    color: ${props => props.theme.colors.white};
+  }
 `
 
 const ButtonTextLink = ({ className, href, title }: ButtonTextLinkProps) => {
-  const [isHovered, setHovered] = useState(false)
-
   const styleProps = useSpring({
-    backgroundPositionY: isHovered ? '100%' : '0%',
-    color: isHovered ? theme.colors.white : theme.colors.darkBlue,
+    from: {
+      opacity: 0,
+      transform: `translateY(-${rem(24)})`,
+    },
+    to: {
+      opacity: 1,
+      transform: 'translateY(0)',
+    },
   })
 
   return (
-    <StyledButton
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      className={className}
-      href={href}
-      style={styleProps}
-    >
+    <StyledButton className={className} href={href} style={styleProps}>
       {title}
     </StyledButton>
   )
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -11,36 +11,38 @@ import Text from '../components/text'
 
 import { META_TITLES, URL_GITHUB, URL_INSTAGRAM } from '../constants'
 
-const StyledButtonTextLink = styled(ButtonTextLink)`
-  align-self: center;
-`
-
 const StyledText = styled(Text)`
   margin-bottom: ${rem(48)};
 `
 
-const ContactPage = () => (
-  <>
-    <SEO title={META_TITLES.CONTACT} />
-    <Section>
-      <Heading>Contact</Heading>
-      <Container>
-        <StyledText>
-          <p>
-            I am available for contract work in Bristol, London and the
-            South-West of the UK. If you would like to get in touch to discuss
-            any opportunities, click the button below to drop me an email.
-          </p>
+const StyledButtonTextLink = styled(ButtonTextLink)`
+  align-self: center;
+`
 
-          <p>
-            Alternativly, view my projects on <a href={URL_GITHUB}>Github</a> or
-            follow me on <a href={URL_INSTAGRAM}>Instagram</a>.
-          </p>
-        </StyledText>
-        <StyledButtonTextLink href="mailto:hello@kansu.co" title="Contact" />
-      </Container>
-    </Section>
-  </>
-)
+const ContactPage = () => {
+  return (
+    <>
+      <SEO title={META_TITLES.CONTACT} />
+      <Section>
+        <Heading>Contact</Heading>
+        <Container>
+          <StyledText>
+            <p>
+              I am available for contract work in Bristol, London and the
+              South-West of the UK. If you would like to get in touch to discuss
+              any opportunities, click the button below to drop me an email.
+            </p>
+
+            <p>
+              Alternativly, view my projects on <a href={URL_GITHUB}>Github</a>{' '}
+              or follow me on <a href={URL_INSTAGRAM}>Instagram</a>.
+            </p>
+          </StyledText>
+          <StyledButtonTextLink href="mailto:hello@kansu.co" title="Contact" />
+        </Container>
+      </Section>
+    </>
+  )
+}
 
 export default ContactPage
