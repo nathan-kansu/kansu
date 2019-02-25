e96441df67ac78365bf342f9131ed2e199d1140d
Nathan Tredgett <nathantredgett@gmail.com>
2019-02-25 18:49:15 +0000

    updated github url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,7 +6,7 @@ export const ANIMATION_REPOSITION_SPEED = 0.3
 export const COLOR_DARK_BLUE_RGBA = [32, 47, 60, 0]
 export const COLOR_GREY_RGBA = [73, 85, 94, 0]
 export const COLOR_WHITE_RGBA = [255, 255, 255, 0]
-export const URL_GITHUB = 'https://github.com/nath-codes'
+export const URL_GITHUB = 'https://github.com/nathan-kansu'
 export const URL_INSTAGRAM = 'https://www.instagram.com/nathan.kansu'
 
 export const ARIA_GITHUB = 'View my projects on Github'
