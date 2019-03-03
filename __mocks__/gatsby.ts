7e4e77e6df6ee5d46a97b5246fdde92ae04fd12b
Nathan Tredgett <nathantredgett@gmail.com>
2019-03-03 18:35:46 +0000

    added Gatbsy mock

---
+++ __mocks__/gatsby.ts
@@ -0,0 +1,14 @@
+import React from 'react'
+const gatsby = jest.requireActual('gatsby')
+
+module.exports = {
+  ...gatsby,
+  graphql: jest.fn(),
+  Link: jest.fn().mockImplementation(({ to, ...rest }) =>
+    React.createElement('a', {
+      ...rest,
+      href: to,
+    })
+  ),
+  StaticQuery: jest.fn(),
+}
